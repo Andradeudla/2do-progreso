fb782c0f715f9bddea8b80e1346728d13806acc1
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-11-21 15:33:59 -0500

    ultimo commit

--- matrices.c
+++ matrices.c
@@ -2,22 +2,24 @@
 int main()
 {
     int n, m;
+
     printf("Ingrese el numero de filas: ");
     scanf("%d", &n);
 
     printf("Ingrese el numero de columnas: ");
     scanf("%d", &m);
 
-    int matrix[n][m];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            matrix[i][j] = (i == j) ? 1 : 0;
-    
-    printf("\n MATRIZ \n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            printf("%d", matrix[i][j]);
+    int matriz[n][m];
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            matriz[i][j] = (i == j) ? 1 : 0;
+
+    printf("\nMATRIZ\n");
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++)
+            printf("%d ", matriz[i][j]);
         printf("\n");
+    }
 
     return 0;
 }
