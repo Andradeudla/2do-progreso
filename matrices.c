f344bf59e2edc0fb6bf3fa3f03271f62ca755738
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-11-21 15:03:39 -0500

    primer commit

---
+++ matrices.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+    int n, m;
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &n);
+
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &m);
+
+    int matrix [n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; i++)
+        {
+            /* code */
+        }
+        
+    }
+    
+
+    return 0;
+}
